1feff955f387b371d136d80f75170a0529d3ab6e
Foxengton <foxengton@yandex.ru>
2025-07-07 04:39:27 +0700

    add logs

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import ScreepsStatsd from "./src/ScreepsStatsd.js";
 
-new ScreepsStatsd(
+const statsd = new ScreepsStatsd(
     process.env.SCREEPS_HOST,
     process.env.SCREEPS_EMAIL,
     process.env.SCREEPS_PASSWORD,
     process.env.SCREEPS_SHARD,
-    process.env.GRAPHITE_PORT_8125_UDP_ADDR,
-).run();
+    process.env.GRAPHITE_PORT_8125_UDP_ADDR || "statsd"
+);
+
+statsd.run();
+
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled rejection:", err);
+});
